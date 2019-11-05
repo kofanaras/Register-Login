73e4493109ba4e1156b849f1a213fcb57f60b7b8
kofanaras <>
2019-11-05 18:46:30 +0200

    register-login-code

---
+++ LoginRegistration/src/com/mk/controller/ProcessLogin.java
@@ -0,0 +1,61 @@
+package com.mk.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.mk.dao.UserDao;
+
+/**
+ * Servlet implementation class ProcessLogin
+ */
+@WebServlet("/processLogin")
+public class ProcessLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProcessLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		HttpSession session=request.getSession();
+		PrintWriter out=response.getWriter();
+		
+		UserDao dao=new UserDao();
+		RequestDispatcher rd=request.getRequestDispatcher("Home.jsp");
+		try {
+			boolean authenticate=dao.authenticate(username, password);
+			if(authenticate) {
+				session.setAttribute("username", username);
+				rd.forward(request, response);
+				
+			}
+			else {
+				rd=request.getRequestDispatcher("login.jsp");
+				rd.include(request, response);
+				out.println("Username and password combination is not valid,please try again.");
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace(out);
+			out.println("Your request cannot be processed at this time, please try later.");
+		}
+	}
+
+}
---
+++ LoginRegistration/src/com/mk/controller/ProcessRegistration.java
@@ -0,0 +1,66 @@
+package com.mk.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mk.dao.UserDao;
+
+/**
+ * Servlet implementation class ProcessRegistration
+ */
+@WebServlet("/processRegistration")
+public class ProcessRegistration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProcessRegistration() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstname=request.getParameter("firstname");
+		String lastname=request.getParameter("lastname");
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		String confirmPass=request.getParameter("confirmPass");
+		
+		RequestDispatcher rd1=request.getRequestDispatcher("register.jsp");
+		RequestDispatcher rd2=request.getRequestDispatcher("login.jsp");
+		PrintWriter out=response.getWriter();
+		
+		UserDao dao = new UserDao();
+		try {
+			boolean isExist=dao.isExist(username);
+			if(isExist) {
+				rd1.include(request, response);
+				out.println("User with username "+username+" already exists.");
+			}else {
+				if(!password.equals(confirmPass)) {
+					rd1.include(request, response);
+					out.println("Password and confirm password dont match");
+				}else {
+					dao.registerUser(firstname, lastname, username, password);
+					rd2.include(request, response);
+					out.println("User registered successfully");
+				}
+			}
+		} catch (Exception e) {
+			out.println("Your request cannot be processed.");
+		}
+		
+	}
+
+}
